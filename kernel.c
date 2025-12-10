cf7e348a8c83c463bed738d28c81e394bb87db7b
diannaaa <diannaaav@gmail.com>
2025-12-10 15:42:27 +0800

    Add common.c and common.h to inplement print functionality

--- kernel.c
+++ kernel.c
@@ -1,4 +1,5 @@
 #include "kernel.h"
+#include "common.h"
 typedef unsigned char uint8_t;
 typedef unsigned int uint32_t;
 typedef uint32_t size_t;
@@ -36,10 +37,9 @@ void putchar(char ch) { sbi_call(ch, 0, 0, 0, 0, 0, 0, 1); }
 void kernel_main(void) {
   memset(__bss, 0, (size_t)__bss_end - (size_t)__bss);
 
-  const char *s = "\n\nHello World!\n";
-  for (int i = 0; s[i] != '\0'; i++) {
-    putchar(s[i]);
-  }
+  printf("\n\nHello %s\n", "World!");
+  printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
+  
   for (;;) {
     __asm__ __volatile__("wfi");
   }
--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ QEMU=qemu-system-riscv32
 CC=clang
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fuse-ld=lld -fno-stack-protector -ffreestanding -nostdlib"
 
-$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c common.c
 
 # start QEMU
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf
