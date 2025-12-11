cb192ee4e147f5c4980db24beee7c509134cbac7
diannaaa <diannaaav@gmail.com>
2025-12-11 13:18:29 +0800

    Remove memset from kernel.c and format the code.

--- kernel.c
+++ kernel.c
@@ -25,13 +25,6 @@ struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
   return (struct sbiret){.error = a0, .value = a1};
 }
 
-void *memset(void *buf, char c, size_t n) {
-  uint8_t *p = (uint8_t *)buf;
-  while (n--)
-    *p++ = c;
-  return buf;
-}
-
 void putchar(char ch) { sbi_call(ch, 0, 0, 0, 0, 0, 0, 1); }
 
 void kernel_main(void) {
@@ -39,7 +32,7 @@ void kernel_main(void) {
 
   printf("\n\nHello %s\n", "World!");
   printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
-  
+
   for (;;) {
     __asm__ __volatile__("wfi");
   }
